74fae36409aceb49c25a7f9d8e69a248d2d0b169
batkhishig <batkhishign55@gmail.com>
2024-10-09 15:18:12 -0500

    address

--- hw3/src/BdulamsurankhorProducer.java
+++ hw3/src/BdulamsurankhorProducer.java
@@ -24,7 +24,7 @@ public class BdulamsurankhorProducer {
 
         String topicName = args[0];
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "129.114.27.180:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
